ee1faeb5332ae97ec6463498e73c6abc6672ba0d
snailfan300 <sydneyperron15@gmail.com>
2026-09-10 17:17:17 -0400

    Update script.js

--- topics/version-control/version-control-workflow/js/script.js
+++ topics/version-control/version-control-workflow/js/script.js
@@ -1,24 +1,38 @@
 /**
- * Title of Project
- * Author Name
- * 
- * HOW EMBARRASSING! I HAVE NO DESCRIPTION OF MY PROJECT!
- * PLEASE REMOVE A GRADE FROM MY WORK IF IT'S GRADED!
- */
+* git example
+* sydney perron
+* 
+* sample code for version control
+* draw me a nice pyramid
+* also i'm copying this by hand because i cannot
+* do it with the apple mouse provided in the lab
+* /
 
 "use strict";
-
-/**
- * OH LOOK I DIDN'T DESCRIBE SETUP!!
-*/
+/** create a canvas, hides cursor
+ */
 function setup() {
-
+    // 640x480 canvas
+    createCanvas(640, 640);
+    // Don't show cursor vvv
+    noCursor();
 }
 
-
-/**
- * OOPS I DIDN'T DESCRIBE WHAT MY DRAW DOES!
-*/
-function draw() {
-
+function draw(){
+    background(0, 0, 0);
+    const levels = 10;
+    for (let level = levels; level > 0; level--){
+        push();
+        const shade = map(level, 1, levels, 10, 255);
+        noStroke();
+        fill(shade, shade, 0);
+        rectMode(CENTER);
+        rect(320, 320, level * 48, level * 48);
+        pop();
+    }
+    push();
+    noStroke();
+    fill(255, 0, 0);
+    ellipse(mouseX, mouseY, 100, 100);
+    pop();
 }
